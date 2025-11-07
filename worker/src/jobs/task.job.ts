790f7707b1f8de04098d9976854f6fa50c7fe840
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2025-11-07 10:20:11 +0100

    feat: implemeneted background listener for task jobs

--- worker/src/jobs/task.job.ts
+++ worker/src/jobs/task.job.ts
@@ -1,6 +1,27 @@
 import type {Job} from 'bullmq';
-import {Task} from "../types/task";
+import {TaskRepository} from "../repositories/TaskRepository";
+import {CreateTaskDTO, TaskID, UpdateTaskDTO} from "../types/task";
 
-export default async function taskJob(job: Job<Task>) {
-  // TODO: handle the task job
+export default async function taskJob(job: Job<CreateTaskDTO | UpdateTaskDTO | TaskID>) {
+  switch (job.name) {
+    case 'createTask':
+      void TaskRepository.create(job.data as CreateTaskDTO)
+
+      break;
+
+    case 'updateTask':
+      const data = job.data as UpdateTaskDTO & { id: TaskID };
+
+      void TaskRepository.updateById(data.id, data)
+
+      break;
+
+    case 'deleteTask':
+      void TaskRepository.deleteById(job.data as TaskID)
+
+      break;
+
+    default:
+      throw new Error(`Unknown job name: ${job.name}`);
+  }
 }
