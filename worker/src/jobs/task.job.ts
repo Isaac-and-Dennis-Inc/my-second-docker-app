7016c5f9c9240dd779d028c82f5cc07e2384b74e
Sondre Benjamin Aasen <699902+sobeaa@users.noreply.github.com>
2025-11-07 09:42:23 +0100

    feat: implemented task types

--- worker/src/jobs/task.job.ts
+++ worker/src/jobs/task.job.ts
@@ -1,5 +1,5 @@
 import type {Job} from 'bullmq';
-import {Task} from "../types/task.type";
+import {Task} from "../types/task";
 
 export default async function taskJob(job: Job<Task>) {
   // TODO: handle the task job
